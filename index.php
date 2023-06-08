e6fd4f828d93a031565a9c0ea7b47a54a4233d90
Cloud <ruben.as532@gmail.com>
2023-06-08 20:10:48 +0200

    Added external texts support non-parametrized

--- index.php
+++ index.php
@@ -31,18 +31,15 @@
     	}
     	printAnalyzedTextResult(count($splitted_text),$phrase_counter,$letter_counter,$longest_word);
     }
-	$texto1 = "Esto es un texto, de: prueba. Tendrá solo 3 oraciones. A ver si funciona.";
-	$esto_es_un_monologazo="Love is awful. It’s awful. It’s painful. It’s frightening. 
-It makes you doubt yourself, judge yourself, distance yourself from the other people in your life. I
-t makes you selfish. It makes you creepy, makes you obsessed with your hair, makes you cruel, makes you say and do things you never thought you would do. 
-It’s all any of us want, and it’s hell when we get there. 
-So no wonder it’s something we don’t want to do on our own. 
-I was taught if we’re born with love then life is about choosing the right place to put it. 
-People talk about that a lot, feeling right, when it feels right it’s easy. But I’m not sure that’s true. 
-It takes strength to know what’s right. And love isn’t something that weak people do. Being a romantic takes a hell of a lot of hope. 
-I think what they mean is, when you find somebody that you love, it feels like hope.";
 
-    analyzeText($texto1);
-    analyzeText($esto_es_un_monologazo);
+
+    $file1 = fopen("texto1.txt", "r") or die("Unable to open file.");
+    $file2 = fopen("texto2.txt", "r") or die("Unable to open file.");
+    $files = ["texto1.txt" => $file1, "texto2.txt" => $file2];
+
+    foreach($files as $key => $file) {
+    	$text =  fread($file,filesize($key));
+    	analyzeText($text);
+    }
 
 ?>
